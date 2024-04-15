5fcfe2ffc5a905659fb6017bdf0c1819e7d52243
azizbelkhouja <belkhoujamohamedaziz@gmail.com>
2024-04-15 16:02:11 +0100

    number guessing game

--- main.c
+++ main.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 
-/*
-Range Validation: You might want to add some validation to ensure that the user enters a valid range. 
-For example, you could check if the minimum number is less than the maximum number.
-
-Input Validation: You should consider adding input validation to ensure that the user enters a valid number 
-when prompted to guess. If the user enters something that is not a number, the program could go into an infinite loop.
-
-Feedback for Out of Range Guesses: Currently, if the user guesses a number outside the specified range, 
-there is no feedback given. You could add a message to inform the user that the guess is out of range and prompt them to guess again.
-
-Optional: Display the Range: It might be helpful to display the range again to the user before prompting them to guess. 
-This can help them remember the boundaries within which they should guess.
-
-Optional: Allow the User to Play Again: After the game ends (whether the user wins or loses), 
-you could ask the user if they want to play again. If they do, reset the game and generate a new random number.
-*/
 int main()
 {
 
     int minimumnumber, maximumnumber;
     int thenumber, usernumber;
+
+    do {
 
     printf("--insert a range-- \n");
     printf("from: ");
@@ -31,17 +18,27 @@ int main()
     printf("to: ");
     scanf("%d", &maximumnumber);
 
+    } while (minimumnumber>maximumnumber);
+
     srand(time(NULL));
 
     thenumber = rand() % (maximumnumber-minimumnumber) + minimumnumber;
 
     //printf("the number you must guess is = %d\n", thenumber);
     
-    printf("--------------\n");
+    printf("\n--------------\n");
+    printf("RANGE %d - %d\n\n", minimumnumber, maximumnumber);
 
+    
     do {
-        printf("insert a number: ");
-        scanf("%d", &usernumber);
+        printf("insert your guess: ");
+        
+        if (scanf("%d", &usernumber) != 1) {
+            while (getchar() != '\n');
+            continue;
+        }
+        
+        if (usernumber>maximumnumber || usernumber<minimumnumber) printf("number out of the range, ");
 
         if (usernumber == thenumber)
         {
@@ -58,7 +55,7 @@ int main()
         }
         
 
-    } while (usernumber != thenumber);
+    } while ((usernumber != thenumber));
 
     return 0;
 }
